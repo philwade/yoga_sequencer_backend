9bcbda99baa2d30f2b98d8bf90da9cdc773c4a61
Phil Wade <pwade@harrislocalgov.com>
2015-04-20 16:11:24 -0400

    Initial commit

---
+++ yoga.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from crossdomain import crossdomain
+
+app = Flask(__name__)
+
+data = {
+    'poses' : [
+        {
+            'id': 0,
+            'name': 'Utthita Trikonasana',
+            'easy_name': 'Triangle',
+            'time': 5,
+        },
+        {
+            'id': 1,
+            'name': 'Utthita Parsvakonasana',
+            'easy_name': 'Extended Side Angle',
+            'time': 5,
+        },
+    ],
+}
+
+@app.route('/api/sequence')
+@crossdomain(origin='*')
+def index():
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
