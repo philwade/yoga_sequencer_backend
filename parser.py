754b2606bdd77b8e8f6164ee8efcdc022beb302d
Philip Wade <phil@philwade.org>
2015-05-04 21:47:33 -0400

    Sort of working database filling, some data cleanup.

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ from sys import argv
 from urllib2 import urlopen
 import json
 from unidecode import unidecode
+from models import *
 
 filename = argv[1]
 
@@ -10,6 +11,9 @@ f = open(filename)
 url_query = 'http://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=imageinfo&iiprop=url&format=json&continue'
 attribution_query = 'http://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=imageinfo&iiprop=extmetadata&format=json&continue'
 
+image_path = 'app/images/asanas/'
+s = create_session()
+
 for line in f:
     fields = line.split('|')
 
@@ -17,8 +21,11 @@ for line in f:
     english = fields[3].strip()
     image_name = fields[4].strip()
 
+    # Sure.
     name = name.decode('utf8')
     name = unidecode(name).encode('ascii')
+    name = name.decode('ascii')
+    name = unidecode(name).encode('utf8')
 
     print name, english, image_name
 
@@ -34,22 +41,46 @@ for line in f:
         response = json.loads(file_url.read())
         attribution = json.loads(image_request_url.read())
 
-        image_url = response['query']['pages']['-1']['imageinfo'][0]['url']
+        try:
+            image_url = response['query']['pages']['-1']['imageinfo'][0]['url']
 
-        description_url = response['query']['pages']['-1']['imageinfo'][0]['descriptionurl']
-        author = attribution['query']['pages']['-1']['imageinfo'][0]['extmetadata']['Artist']['value']
-        license = attribution['query']['pages']['-1']['imageinfo'][0]['extmetadata']['LicenseUrl']['value']
+            description_url = response['query']['pages']['-1']['imageinfo'][0]['descriptionurl']
+            author = attribution['query']['pages']['-1']['imageinfo'][0]['extmetadata']['Artist']['value']
+            license = attribution['query']['pages']['-1']['imageinfo'][0]['extmetadata']['LicenseUrl']['value']
 
-        image = urlopen(image_url)
-        image_name = name.replace(' ', '')
-        file_extenstion = image_url.split('.')[-1]
+            image = urlopen(image_url)
+            image_name = name.replace(' ', '')
+            file_extenstion = image_url.split('.')[-1]
 
-        f = open(image_name + '.' + file_extenstion, 'wb')
-        f.write(image.read())
-        f.close()
+            full_image_path = image_path + image_name + '.' + file_extenstion
+            f = open(full_image_path, 'wb')
+            f.write(image.read())
+            f.close()
+
+            p = Pose(
+                name = name,
+                simplename = english,
+            )
+
+            s.add(p)
+
+            pi = PoseImage(
+                url = full_image_path,
+                author = author,
+                license = license,
+                further_attribution = description_url,
+                pose = p,
+            )
+
+            s.add(pi)
+
+        except KeyError:
+            #dunno what to do here...
+            print "BAD KEY"
 
         print image_url
         print author
         print license
 
+s.commit()
 
